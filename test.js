ae048aefd749b02e1d81e93e0adaff6cc807594b
Kevin Martensson <kevinmartensson@gmail.com>
2017-07-24 02:15:40 +0200

    Meta tweaks

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
+import fs from 'fs';
 import execa from 'execa';
 import test from 'ava';
 
-test('show help screen', async t => {
-	const ret = await execa('./cli.js', ['--help']);
-	t.regexTest(/Safely force create symlinks/, ret.stdout);
+test('symlink a file', async t => {
+	await execa('./cli.js', [__filename, 'tmp.js']);
+	const file = await fs.realpathSync('tmp.js');
+	t.is(file, __filename);
+	await fs.unlinkSync('tmp.js');
 });
