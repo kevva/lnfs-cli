36713bc6ba1bcd099bab1caa27d805c1d0db2256
Noam Okman <noamokman@gmail.com>
2017-08-13 14:16:23 +0300

    added type support

--- cli.js
+++ cli.js
@@ -4,20 +4,48 @@ const path = require('path');
 const lnfs = require('lnfs');
 const meow = require('meow');
 
+const typeOptions = ['file', 'dir', 'junction'];
+
 const cli = meow(`
 	Usage
-	  $ lnfs <file> <target>
+	  $ lnfs [--type=<string>] <file> <target>
+
+	Options
+				--type, -t  Specify a symlink type [${typeOptions.join('|')}]
+				--dir, -d Alias for --type dir
+				--junction, -j Alias for --type junction
 
 	Example
 	  $ lnfs foo.txt bar.txt
 	  /home/unicorn/bar.txt -> /home/unicorn/foo.txt
-`);
+`, {
+	alias: {
+		t: 'type',
+		j: 'junction',
+		d: 'dir'
+	}
+});
 
 if (cli.input.length < 2) {
 	console.error('Source file and target required');
 	process.exit(1);
 }
 
-lnfs(cli.input[0], cli.input[1]).then(() => {
+let type = cli.flags.type;
+
+if (type && typeOptions.indexOf(type) === -1) {
+	console.error(`\`type\` option is invalid. should be one of these values: ${typeOptions.join()}`);
+	process.exit(1);
+}
+
+if (cli.flags.dir) {
+	type = 'dir';
+}
+
+if (cli.flags.junction) {
+	type = 'junction';
+}
+
+lnfs(cli.input[0], cli.input[1], type).then(() => {
 	console.log(`${path.resolve(cli.input[1])} -> ${path.resolve(cli.input[0])}`);
 });
